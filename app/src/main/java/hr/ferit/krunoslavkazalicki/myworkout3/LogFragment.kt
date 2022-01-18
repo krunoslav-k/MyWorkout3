cce57c9d4ddd083c44276bf70d505638664180b4
Krunoslav Kazalicki <krunoslavk@hotmail.com>
2022-01-18 15:19:21 +0100

    fragments code added

--- app/src/main/java/hr/ferit/krunoslavkazalicki/myworkout3/LogFragment.kt
+++ app/src/main/java/hr/ferit/krunoslavkazalicki/myworkout3/LogFragment.kt
@@ -5,55 +5,59 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioGroup
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [LogFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LogFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_log, container, false)
+        val view = inflater.inflate(R.layout.fragment_log, container, false)
+
+        val repsEditText = view.findViewById<EditText>(R.id.reps_etnum)
+        val setsEditText = view.findViewById<EditText>(R.id.sets_etnum)
+        val durationEditText = view.findViewById<EditText>(R.id.duration_etnum)
+        val intensityEditText = view.findViewById<EditText>(R.id.intensity_etnum)
+        val muscleGroupRadioGroup = view.findViewById<RadioGroup>(R.id.muscleGroup_rg)
+        val logSetWorkoutButton = view.findViewById<Button>(R.id.logSetWorkout_btn)
+        val logTimedWorkoutButton = view.findViewById<Button>(R.id.logTimedWorkout_btn)
+
+        var reps = repsEditText.text.toString().toInt()
+        var sets = setsEditText.text.toString().toInt()
+        var duration = durationEditText.text.toString().toInt()
+        var intensity = intensityEditText.text.toString().toInt()
+        var muscleGroup = "unset"
+
+        muscleGroupRadioGroup.setOnCheckedChangeListener { group, checkedId ->
+            muscleGroup = when(checkedId){
+                R.id.arms_rb -> "arms"
+                R.id.legs_rb -> "legs"
+                R.id.upperBody_rb -> "upper body"
+                R.id.lowerBody_rb -> "lower body"
+                else -> "unset"
+            }
+        }
+
+        logSetWorkoutButton.setOnClickListener(){
+
+        }
+
+        logTimedWorkoutButton.setOnClickListener(){
+
+        }
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LogFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LogFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    fun calculateSetEffort(reps: Int, sets: Int, muscleGroup: String){
+
+
+
     }
+
 }
--- app/src/main/java/hr/ferit/krunoslavkazalicki/myworkout3/ProfileFragment.kt
+++ app/src/main/java/hr/ferit/krunoslavkazalicki/myworkout3/ProfileFragment.kt
@@ -1,59 +1,56 @@
 package hr.ferit.krunoslavkazalicki.myworkout3
 
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioGroup
+import android.widget.TextView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+        val usernameTextView = view.findViewById<TextView>(R.id.username_tv)
+        val numberOfWokroutsTextView = view.findViewById<TextView>(R.id.numOfWorkouts_tv)
+        val weightEditText = view.findViewById<EditText>(R.id.weight_etnum)
+        val heightEditText = view.findViewById<EditText>(R.id.height_etnum)
+        val genderRadioGroup = view.findViewById<RadioGroup>(R.id.gender_rg)
+        val ageEditText = view.findViewById<EditText>(R.id.age_etnum)
+        val bodyFatPercentageEditText = view.findViewById<EditText>(R.id.bodyFatPercentage_etnum)
+        val musclePercentageEditText = view.findViewById<EditText>(R.id.musclePercentage_etnum)
+        val caloriesIntakeEditText = view.findViewById<EditText>(R.id.caloriesIntake_etnum)
+        val saveChangesButton = view.findViewById<Button>(R.id.saveChanges_btn)
+
+        var username = usernameTextView.text.toString()
+        var numberOfWorkouts = numberOfWokroutsTextView.text.toString().toInt()
+        var weight = weightEditText.text.toString().toFloat()
+        var height = heightEditText.text.toString().toFloat()
+        var gender = "unset"
+
+        genderRadioGroup.setOnCheckedChangeListener { group, checkedId ->
+             gender = when(checkedId){
+                R.id.male_rb -> "male"
+                R.id.female_rb -> "female"
+                else -> "unset"
             }
+        }
+
+        var age = ageEditText.text.toString().toInt()
+        var bodyFatPercentage = bodyFatPercentageEditText.text.toString().toFloat()
+        var musclePercentage = musclePercentageEditText.text.toString().toFloat()
+        var caloriesIntake = caloriesIntakeEditText.text.toString().toFloat()
+
+        saveChangesButton.setOnClickListener(){}
+
+        return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 }
